45c223279c010fb01d414a13bd2c5eddbe921a95
Egemen Oner <egemen.oner@yandex.com>
2020-10-27 17:50:38 +0300

    User should be able to create a survey now

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,26 @@
 class SurveysController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @city = City.find(params[:city_id])
+    @survey = Survey.new
+  end
+
+  def create
+    @city = City.find(params[:city_id])
+    @survey = Survey.new(survey_params)
+    @survey.city = @city
+    @survey.user = current_user
+
+    if @survey.save
+      redirect_to city_path(@city)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:question)
+  end
 end
