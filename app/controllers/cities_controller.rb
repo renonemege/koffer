cf0771f9748929807d6afa588547894d8e02f1ad
Esin Adsoy <esin.adsoy@gmail.com>
2020-10-28 00:16:16 +0300

    added search bar and cities show page

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,10 @@
 class CitiesController < ApplicationController
   def index
-    @cities = City.all
-
+    if params[:query].present?
+      @cities = City.where(title: params[:query])
+    else
+      @cities = City.all
+    end
     @markers = @cities.geocoded.map do |city|
       {
         lat: city.latitude,
@@ -13,5 +16,15 @@ class CitiesController < ApplicationController
 
   def show
     @city = City.find(params[:id])
+    @review = Review.new
+    @user = current_user
+
+    city = @city.geocode
+    @markers =
+      [{
+        lat: @city.latitude,
+        lng: @city.longitude,
+        image_url: helpers.asset_url('yellowmarker2.png')
+      }]
   end
 end
