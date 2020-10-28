b6554cc091fa6a07607afd9df5cf5af672b5ab3c
Esin Adsoy <esin.adsoy@gmail.com>
2020-10-28 13:18:50 +0300

    installed cloudinary and attached photos to seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :surveys
   has_many :user_cities
   has_many :responsess
+  has_one_attached :photo
 
   # validates :email, :first_name, :last_name, :username, :password, :occupation,
   # :description, :image_url, :score, presence: true
